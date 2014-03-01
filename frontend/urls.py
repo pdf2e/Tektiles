c397f288eb7d610bcd7a7342591e4318d6088f47
Angelo Todaro <angelotodaro92@gmail.com>
2014-03-01 15:52:40 -0600

    Fixed Link to Index

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,5 +5,6 @@ from frontend import views
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^about/$', views.about, name='about'),
-        url(r'^contact/$', views.contact, name='contact')
+        url(r'^contact/$', views.contact, name='contact'),
+        url(r'^$', 'index', name='index')
  )
